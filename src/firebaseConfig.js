dcf10cb629d23413eeadd5fd3211706c976d710c
patrikcepr <wkidmusic@gmail.com>
2022-01-23 02:30:44 +0100

    app working, first deployment

---
+++ src/firebaseConfig.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from 'firebase/app';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6Y6xTF3EiDTop37Vk-0gPBrDrPy5YJYE',
+
+  authDomain: 'oict-react-typescript.firebaseapp.com',
+
+  projectId: 'oict-react-typescript',
+
+  storageBucket: 'oict-react-typescript.appspot.com',
+
+  messagingSenderId: '382535468418',
+
+  appId: '1:382535468418:web:08f05242696339867fbb9b',
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
